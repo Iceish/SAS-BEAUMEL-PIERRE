264d5748b2995b3d894a08655d75a7d0b0014834
enzogzz <enzotj.glz@gmail.com>
2022-01-27 22:47:21 +0100

    Footer mobile & desktop. (prototype)

--- resources/views/web/static/layout/footer.blade.php
+++ resources/views/web/static/layout/footer.blade.php
@@ -1,3 +1,10 @@
 <footer>
+    <ul role="list">
+        <li><a href="">Contact us</a></li>
+        <li><a href="">CGU</a></li>
+        <li><a href="">Foo</a></li>
+        <li><a href="">Foo
+            </a></li>
+    </ul>
     <p>All right <i>pas encore</i> reserved 2021 ©</p>
 </footer>
