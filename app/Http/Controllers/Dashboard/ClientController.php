6d688c4e89a64d1ac32b9511f8804bd28189e498
Nyamort <rmurat.murat@gmail.com>
2022-01-31 13:20:55 +0100

    ClientController fix

--- app/Http/Controllers/Dashboard/ClientController.php
+++ app/Http/Controllers/Dashboard/ClientController.php
@@ -2,7 +2,109 @@
 
 namespace App\Http\Controllers\Dashboard;
 
-class ClientController
-{
+use App\Http\Controllers\Controller;
+use App\Http\Requests\SearchRequest;
+use App\Models\Client;
+use App\Http\Requests\StoreClientRequest;
+use App\Http\Requests\UpdateClientRequest;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
 
+class ClientController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @param SearchRequest $request
+     * @return Application|Factory|View
+     */
+    public function index(SearchRequest $request): Application|Factory|View
+    {
+        $validated= $request->validated();
+        $searchText = $validated["search"] ?? "";
+
+        $client = Client::query()
+            ->whereLike(["email","name"],$searchText)
+            ->paginate(25);
+        return view("web.dashboard.sections.client.index",
+            compact("client")
+        );
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return Application|Factory|View
+     */
+    public function create(): Application|Factory|View
+    {
+        return view("web.dashboard.sections.client.create");
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param StoreClientRequest $request
+     * @return RedirectResponse
+     */
+    public function store(StoreClientRequest $request): RedirectResponse
+    {
+        $validated = $request->validated();
+        Client::create($validated)->save();
+        return redirect()->route("dashboard.client.index")->with("success",__("messages.client.create.success"));
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param Client $client
+     * @return Application|Factory|View
+     */
+    public function show(Client $client): View|Factory|Application
+    {
+        return view("web.dashboard.sections.client.show",
+            compact("client")
+        );
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param Client $client
+     * @return Application|Factory|View
+     */
+    public function edit(Client $client): Application|Factory|View
+    {
+        return view("web.dashboard.sections.client.edit",
+            compact($client)
+        );
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param UpdateClientRequest $request
+     * @param Client $client
+     * @return RedirectResponse
+     */
+    public function update(UpdateClientRequest $request, Client $client): RedirectResponse
+    {
+        $validated = $request->validated();
+        $client->update($validated);
+        return redirect()->route("dashboard.users.index")->with("success",__("messages.client.update.success"));
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param Client $client
+     * @return RedirectResponse
+     */
+    public function destroy(Client $client): RedirectResponse
+    {
+        $client->delete();
+        return redirect()->route("Dashboard.Users.View")->with('message',__("messages.client.delete.success"));
+    }
 }
