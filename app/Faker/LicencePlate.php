75c8a6dfd08f0f151006b34bc82fba7a046b5ba2
Nyamort <rmurat.murat@gmail.com>
2022-01-31 13:29:09 +0100

    LicencePlate fix

---
+++ app/Faker/LicencePlate.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Faker;
+
+use Faker\Provider\Base;
+
+class LicencePlate extends Base
+{
+    /**
+     * Generate a random licence plate
+     * @return string
+     */
+    public function licence_plate(): string
+    {
+        $licence_plate = null;
+        for($i =0;$i<2;$i++){
+            $licence_plate .= static::randomLetter();
+        }
+        for($i =0;$i<3;$i++){
+            $licence_plate .= static::randomNumber();
+        }
+        for($i =0;$i<2;$i++){
+            $licence_plate .= static::randomLetter();
+        }
+        return $licence_plate;
+    }
+}
