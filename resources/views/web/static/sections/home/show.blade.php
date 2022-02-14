16611e8606fd1d3606ffbfefbd78d436a7d615e5
CodingStrqf <l.ruy42@orange.fr>
2022-02-14 09:54:27 +0100

    remplissage tres grossier des pages

--- resources/views/web/static/sections/home/show.blade.php
+++ resources/views/web/static/sections/home/show.blade.php
@@ -1,5 +1,13 @@
 @extends('web.static.layout')
 
 @section('main')
-    <p>Home page.</p>
+    <h1>Votre capital nutrition animale</h1>
+    <div>
+        <p>
+            Activités d’agrofourniture, conseil, préconisation, distribution des intrants (fertilisants, produits phytosanitaires, semences aliments pour animaux)
+        </p>
+        <p>
+            Commerce des céréales oléagineux et protéagineux, collecte des productions, stockage, commercialisation sur le marché intérieur.
+        </p>
+    </div>
 @endsection
--- resources/views/web/static/sections/partners/show.blade.php
+++ resources/views/web/static/sections/partners/show.blade.php
@@ -1,5 +1,48 @@
 @extends('web.static.layout')
 
 @section('main')
-    <p>Partners page.</p>
+    <h1>Partenaire</h1>
+    <h2>L’entreprise Philicot</h2>
+    <h3>Une PME familiale fortement impliquée dans l’est de la France</h3>
+    <p>
+        Chez PHILICOT,  la fabrication et la distribution d’aliments pour animaux d’élevage est d’abord une histoire de compétences et de proximité relationnelle. Cela fait plus d’un demi-siècle que la réactivité et la performance de notre PME familiale est au service des éleveurs. Notre expérience et notre forte implication dans les différentes filières des ruminants, porc, lapins, volailles, nous ont permis de développer des produits originaux et des services valorisant votre production et votre entreprise.
+
+        Savoir vous écouter, vous accompagner sur le terrain, répondre toujours plus précisément à vos attentes, satisfaire aux exigences des consommateurs restent pour nous des priorités au quotidien.
+
+        Pour nous tous, chaque jour qui passe est un challenge capital, presque un défi que nous sommes fiers de relever à vos côtés. Parce que c’est avec vous que nous avons toujours choisi d’avancer <br>
+        Joseph et Emile NICOT
+    </p>
+
+    <h3>Une proximité, comme une responsabilité régionale</h3>
+    <p>
+        Lorsqu’en 1948, Philibert NICOT a crée PHILICOT, c’était pour fournir aux éleveurs des produits élaborés, capables de répondre aux spécialités de leur productions. Joseph et Philippe NICOT, fils de Philibert, eux-mêmes passionnés par ce métier, n’hésitent pas à aller plus loin et institutionnalisent cette volonté d’innovation qui fait l’originalité de l’entreprise et la qualité de ses produits.
+        Aujourd’hui plus de 100 personnes sur une vingtaine de départements assurent à près de 5000 clients éleveurs et distributeurs, un service de proximité de qualité, productif et réactif.
+        Véritablement impliqué aux côtés des éleveurs, connaissant bien chacun de leurs impératifs,PHILICOT a développé la proximité relationnelle comme une responsabilité régionale. Avec ses sites de production et de représentation, le service Philicot rayonne sur 20 départements, avec le relais efficace de tous nos distributeurs.
+        PHILICOT, C’EST TOUTE LA FORCE D’UNE EQUIPE DE PROFESSIONNELS CONVAINCUS ET IMPLIQUES A VOS COTES
+        Démarré il y a 60 ans, PHILICOT commercialisent dans les années 1990, 80 000 T d’aliments du bétails, puis 150 000 T début des années 2000, pour passer le cap des 200 000 T en 2004. Aujourd’hui, PHILICOT sur l’ensemble de ses sites de production, commercialise 260 000 T, ce qui en fait l’un des principaux fabricants d’aliments de votre région.
+        Plus de 100 personnes au quotidien sur l’ensemble des sites.
+        L’ensemble des différents sites fonctionne de manière autonome dans son fonctionnement au quotidien mais bénéficie des services PHILICOT CHAGNY dans ses fonctions : d’achat et transport des matières premières, de formulation des gammes aliments, du service laboratoire, du service facturation et suivi clients, du service comptabilité.
+    </p>
+    <p>
+        Historique <br>
+        1948 Démarrage de l’activité ALIMENTS DU BETAILS à Chagny
+        1957 Acquisition des Ets GARESSUS à Morteau, site où se trouve le dépôt PHILICOT MORTEAU, ainsi que le magasin des éleveurs.
+        1980 - 1983 Embauches importantes de technico-commerciaux pour développer l’activité aliments en Bourgogne, Franche-Comté et début du Rhône Alpes.
+        1992 Acquisition des Ets LEPINE à Clairvaux Les Lacs, site actuel de production de  PHILICOT FRANCHE-COMTE. A son acquisition, ce site produisait annuellement 6000 T d’aliments du bétail, aujourd’hui, il en produit 30 000 T. En 2006, la construction d’un bâtiment de stockage à plat de céréales complète l’organisation industrielle de ce site.
+        1999 Acquisition des Ets LAFARGE à Cléon d’Antran dans la Drôme. Ce site produit essentiellement des aliments Porcs et Ovins-Caprins pour une production annuelle de 30 000 T.
+        2002 Création d’une activité aliment MASH à Ste Agathe la Boutresse dans la Loire en partenariat avec un client distributeur.
+        2005 - 2006 Création d’une usine à Saint-Flour dans le Cantal pour les Bovins Lait et Viande en présentation MASH (aliments et fibreux).
+        2007 Acquisition de la SOCIAG à Bollene dans le Vaucluse. Ce site produit essentiellement des aliments Volailles et Ovins-Caprins pour une production annuelle de 35 000 T.
+        Acquisition des Ets ARMATAFFET à Annecy en Haute Savoie
+    </p>
+
+    <h2>L'entreprise Galloway</h2>
+    <h3>Nutrition equine</h3>
+    <p>
+        GALLOWAY est une marque de la société PHILICOT SOCIAG, usines spécialisées dans la fabrication d’aliments granulés et floconnés, avec des formulations stabilisées. C’est dans le souci de vous proposer la meilleure qualité que nous mettons tout notre savoir dans la sélection des matières premières ayant toutes un intérêt pour l’alimentation de vos chevaux. Ainsi nous vous apportons la sécurité d’un alimentation de premier choix grâce à une contrôle strict sur les matières premières ainsi que sur le produit fini.
+        Les produits de la Gamme GALLOWAY peuvent vous être livrés en sacs de 25 Kg. Nous pouvons également vous livrer en vrac.
+        GALLOWAY est la marque qui revendique sa spécificité régionale en adaptant ses formules à son terroir tout en étant à la pointe de la nutrition équine.
+        GALLOWAY Le partenaire de votre passion.
+        www.galloway-nutrition.com
+    </p>
 @endsection
