5ba9946de43633fe3b87390622fbf78bfdfabbcb
enzo <enzotj.glz@gmail.com>
2022-02-14 13:08:05 +0100

    Role add button. (Test for remote cpanel)

--- resources/views/web/dashboard/sections/roles/index.blade.php
+++ resources/views/web/dashboard/sections/roles/index.blade.php
@@ -1,7 +1,11 @@
 @extends('web.dashboard.layout')
 
 @section('main')
+    <h2>Roles</h2>
     @foreach($roles as $role)
         {{ $role->name }}
     @endforeach
+
+    <h2>Add new role</h2>
+    <a href="{{ route('dashboard.roles.create') }}">Add new role.</a>
 @endsection
