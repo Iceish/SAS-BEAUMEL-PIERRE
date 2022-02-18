9acd517f2bcad40e784df1cff51d686eabd2f8fa
enzo <enzotj.glz@gmail.com>
2022-02-18 16:49:31 +0100

    Button fix. & Remove old custom table.

--- resources/views/web/dashboard/sections/users/index.blade.php
+++ resources/views/web/dashboard/sections/users/index.blade.php
@@ -1,12 +1,11 @@
 @extends('web.dashboard.layout')
 
+@section('tag','users')
+
 @section('main')
 
     <h2>All users</h2>
-
-{{--    <x-CustomTable contentName="users" :content="$users" keys="id/Id name/Name password/Password :getRoleNames/Roles" />--}}
+    <a class="btn" href="{{ route('dashboard.users.create') }}">Create a new user</a>
     <x-Table.Table :content="$users->items()" columns="Id|id name Roles|roles:name" route="dashboard.users.show:user"/>
 
-    <h2>Add new users</h2>
-    <a href="{{ route('dashboard.users.create') }}">Add user</a>
 @endsection
