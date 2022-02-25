197bce0e1cb2a7aecea27d48cc5b1cf91e8b1f5f
Nyamort <rmurat.murat@gmail.com>
2022-02-25 17:36:19 +0100

    show blade

--- resources/views/web/static/sections/home/show.blade.php
+++ resources/views/web/static/sections/home/show.blade.php
@@ -10,47 +10,6 @@
             Commerce des céréales oléagineux et protéagineux, collecte des productions, stockage, commercialisation sur le marché intérieur.
         </p>
     </div>
-    <div id="myModal" class="modal">
-        <div class="modal-content">
-            <p>
-                Les cookies sont importants pour le fonctionnement de notre site.
-                Certains sont obligatoires.
-            </p>
-            <input id="btnTout" class="btn btn--primary" type="submit" value="Accepter">
-        </div>
-    </div>
-
-    <script>
-        function getCookie(nom){
-            let cookieArr = document.cookie.split(";");
-            for (let i =0; i< cookieArr.length; i++){
-                let cookPair = cookieArr[i].split("=");
-                if (' '+nom === cookPair[0]){
-                    return decodeURIComponent(cookPair[1]);
-                }
-            }
-            return null;
-        }
-        let repCookie = getCookie('accept');
-
-        if ( repCookie === "non" || repCookie == null) {
-            document.cookie = 'accept=non';
-            let modal = document.getElementById("myModal");
-            modal.style.display = "block";
-            window.onclick = function (event) {
-                if (event.target === modal) {
-                    modal.style.display = "none";
-                }
-            }
-
-            let btnTout = document.querySelector("#btnTout");
-            btnTout.onclick = function () {
-                document.cookie = 'accept=oui';
-                modal.style.display = "none";
-            }
-
-        }
-    </script>
 @endsection
 
 @push('js')
