b15cf51f21895daaaf6aa0a9179ec0a37902edb9
enzo <enzotj.glz@gmail.com>
2022-02-26 14:45:56 +0100

    Fix 9ae9b31400a599668b718838313165a100f2a04c.

--- database/migrations/2022_02_24_192534_create_language_language_line_table.php
+++ database/migrations/2022_02_24_192534_create_language_language_line_table.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateLanguageLanguageLinesTable extends Migration
+class CreateLanguageLanguageLineTable extends Migration
 {
     /**
      * Run the migrations.
