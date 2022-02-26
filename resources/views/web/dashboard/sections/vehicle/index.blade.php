0b161d1a91774eb042ccb48b84fb7d074189c412
Nyamort <rmurat.murat@gmail.com>
2022-02-26 19:23:58 +0100

    fix table

--- resources/views/web/dashboard/sections/vehicle/index.blade.php
+++ resources/views/web/dashboard/sections/vehicle/index.blade.php
@@ -3,5 +3,5 @@
 @section('main')
     <h2>Vehicles</h2>
     <a class="btn" href="{{ route('dashboard.vehicles.create') }}">Add a new vehicle</a>
-    <x-Table.Table :content="$vehicles" columns="LicencePlate|licence_plate Revision|revision_date" route="dashboard.vehicles.show:vehicle"/>
+    <x-Table.Table :content="$vehicles" columns="Licence_plate|licence_plate Revision|revision_date" route="dashboard.vehicles.show:vehicle"/>
 @endsection
