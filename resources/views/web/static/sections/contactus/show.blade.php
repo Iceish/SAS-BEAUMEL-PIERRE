2c3442c0090b382c2f116afc0fee05b63c54e86e
Nyamort <rmurat.murat@gmail.com>
2022-03-07 08:47:02 +0100

    tickets

--- resources/views/web/static/sections/contactus/show.blade.php
+++ resources/views/web/static/sections/contactus/show.blade.php
@@ -16,15 +16,15 @@
             <h4>Contact us</h4>
             <div class="field">
                 <label for="email">Email</label>
-                <input name="email" id="email" type="email" placeholder="{{ __('form.placeholder.from') }}" autocomplete="off" value="{{ old('email') }}" required/>
+                <input name="from" id="email" type="email" placeholder="{{ __('form.placeholder.email') }}" autocomplete="off" value="{{ old('email') }}" required/>
             </div>
             <div class="field">
                 <label for="subject">Subject</label>
-                <input name="sibject" id="subject" type="text" placeholder="{{ __('form.placeholder.subject') }}" autocomplete="off" value="{{ old('subject') }}" required/>
+                <input name="subject" id="subject" type="text" placeholder="{{ __('form.placeholder.subject') }}" autocomplete="off" value="{{ old('subject') }}" required/>
             </div>
             <div class="field">
                 <label for="content">Message</label>
-                <textarea name="content" id="content" placeholder="{{ __('form.placeholder.content') }}" maxlength="255" required>{{old('content')}}</textarea>
+                <textarea name="content" id="content" placeholder="{{ __('form.placeholder.textarea') }}" maxlength="255" required>{{old('content')}}</textarea>
             </div>
             <input class="btn" type="submit" value="{{ ucfirst(__('word.submit')) }}"/>
         </form>
