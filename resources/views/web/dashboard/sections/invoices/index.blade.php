b6dfaa82aab1a135bfd1c743436b1464b3624026
enzo <enzotj.glz@gmail.com>
2022-03-13 16:30:46 +0100

    Invoice index style.

--- resources/views/web/dashboard/sections/invoices/index.blade.php
+++ resources/views/web/dashboard/sections/invoices/index.blade.php
@@ -4,6 +4,16 @@
 
 @section('main')
     <x-utils.backBtn/>
-    <a class="btn" href="{{ route('dashboard.invoices.clients.index') }}">{{ ucfirst(trans_choice('custom/words.client', true)) }}</a>
-    <a class="btn" href="{{ route('dashboard.invoices.providers.index') }}">{{ ucfirst(trans_choice('custom/words.provider', true)) }}</a>
+    <h2>{{ ucfirst(trans_choice('custom/words.invoice', false)) }}</h2>
+    <div class="cards">
+        <a class="cards__item" href={{ route('dashboard.invoices.clients.index') }}>
+            <h3><i class="fa-solid fa-2x fa-cash-register"></i> {{ ucfirst(trans_choice('custom/words.client', true)) }}</h3>
+            <p>{{ __('custom/messages.informative.invoice.client') }}</p>
+        </a>
+        <a class="cards__item" href="{{ route('dashboard.invoices.providers.index') }}">
+
+            <h3><i class="fa-solid fa-2x fa-truck-pickup"></i> {{ ucfirst(trans_choice('custom/words.provider', true)) }}</h3>
+            <p>{{ __('custom/messages.informative.invoice.provider') }}</p>
+        </a>
+    </div>
 @endsection
