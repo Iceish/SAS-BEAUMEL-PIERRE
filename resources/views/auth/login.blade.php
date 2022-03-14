abbfaf9bce39112f84c4a9251e9d8ad1acf69d01
Nyamort <rmurat.murat@gmail.com>
2022-03-14 10:46:22 +0100

    fix style btn password

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -20,8 +20,10 @@
             <label for="passwordInput">
                 {{ ucfirst(__('custom/words.data.input.password.default.label')) }}
             </label>
-            <input id="passwordInput" name="password" type="password" placeholder="&#8226;&#8226;&#8226;&#8226;&#8226;&#8226;&#8226;" autocomplete="off"/>
-            <i class="fa-solid fa-eye" id="togglePassword" style="margin-left: -30px; cursor: pointer;"></i>
+            <div>
+                <input id="passwordInput" style="padding-right: 30px" name="password" type="password" placeholder="&#8226;&#8226;&#8226;&#8226;&#8226;&#8226;&#8226;" autocomplete="off"/>
+                <i class="fa-solid fa-eye" id="togglePassword" style="margin-left: -30px; cursor: pointer;"></i>
+            </div>
         </div>
         <div class="field">
             <label for="rememberInput">
